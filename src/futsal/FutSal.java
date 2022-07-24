87e9877ca3754d001a3f4d83df78d8c771cb6c87
Miquel Fajardo <miquel.fajardo@protonmail.com>
2022-07-24 09:51:52 +0200

    Primer commit

---
+++ src/futsal/FutSal.java
@@ -0,0 +1,14 @@
+package futsal;
+
+/**
+ * Aplicació per portar el control d'un equip de futbol sala
+ * @author Miquel Fajardo <miquel.fajardo@protonmail.com>
+ */
+public class FutSal {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
